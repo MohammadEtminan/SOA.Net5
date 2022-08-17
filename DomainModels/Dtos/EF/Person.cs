6dbbad81e7bd1f243de207504a89c82871750828
MohammadEtminan <100367714+MohammadEtminan@users.noreply.github.com>
2022-08-17 06:19:09 +0430

    Creates Fundamentals

---
+++ DomainModels/Dtos/EF/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels.Dtos.EF
+{
+    public class Person
+    {
+        [Key]
+        public int id { get; set; }
+
+        [MaxLength(40)]
+        public string FName { get; set; }
+
+        [MaxLength(40)]
+        public string LName { get; set; }
+
+        [MaxLength(3)]
+        public int Age { get; set; }
+    }
+}
---
+++ Repositories/Contracts/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Contracts
+{
+    interface IRepository<K_DbContext, T_Entity, U_PrimaryKey>
+                                                                where T_Entity : class
+                                                                where K_DbContext : DbContext
+    {
+        K_DbContext Context { get; set; }
+        DbSet<T_Entity> DbSet { get; set; }
+        List<T_Entity> Select();
+        bool Insert(T_Entity entity);
+        bool Update(T_Entity entity);
+        bool Delete(U_PrimaryKey id);
+        bool Delete(T_Entity entity);
+    }
+}
